c847fc64d31e601401da6984f4edfc03cdf6a2d8
Harish0705 <harish.balachandar@nzme.co.nz>
2024-08-08 15:15:31 +1200

    implement search by name, company and featured status

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,7 +1,22 @@
 import { Product } from "../model/productsSchema.js";
 
 export const getAllProducts = async (req, res) => {
-  const query = req.query
-  const products = await Product.find(query);
-  return res.status(200).json({ products, nbHits:products.length });
+  const { isProductfeatured, productCompany, productName } = req.query;
+  console.log({ "Request query": req.query });
+  const queryObject = {};
+  // search products based on featured status
+  if (isProductfeatured) queryObject.isProductfeatured = isProductfeatured;
+  console.log({ "query object featured": queryObject });
+
+  // search products based on company name
+  if (productCompany) queryObject.productCompany = productCompany;
+  console.log({ "query object company": queryObject });
+
+  // search products based on product name-set options to i for case-insesitve
+  if (productName)
+    queryObject.productName = { $regex: productName, $options: "i" };
+  console.log({ "query object productName": queryObject });
+
+  const products = await Product.find(queryObject);
+  return res.status(200).json({ products, nbHits: products.length });
 };
